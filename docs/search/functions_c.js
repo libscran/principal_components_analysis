55bbaefb43981a889db3d6c522b3be96ac6b6917
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2024-07-30 17:46:55 +0000

    Deploying to gh-pages from @ libscran/scran_pca@8bd09b64cd65d43016e6ea30157a877df7dc3d8a 🚀

--- docs/search/functions_c.js
+++ docs/search/functions_c.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['realize_0',['realize',['https://ltla.github.io/CppIrlba/classirlba_1_1MockMatrix.html#ab7a59ec0ae28cab7f6d5443d6f138635',1,'irlba::MockMatrix']]],
-  ['retrieve_5fcompressed_5fsparse_5fcontents_1',['retrieve_compressed_sparse_contents',['https://tatami-inc.github.io/tatami/namespacetatami.html#ad723a47ee2e03e4213cd07fda8710e65',1,'tatami']]],
+  ['retrieve_5fcompressed_5fsparse_5fcontents_1',['retrieve_compressed_sparse_contents',['https://tatami-inc.github.io/tatami/namespacetatami.html#a6b95eab5d7370f965e4d6a9b9a9f3393',1,'tatami']]],
   ['retrieve_5ffragmented_5fsparse_5fcontents_2',['retrieve_fragmented_sparse_contents',['https://tatami-inc.github.io/tatami/namespacetatami.html#ab5688af2360372f1535d6d68b2c14de6',1,'tatami']]],
   ['rows_3',['rows',['https://ltla.github.io/CppIrlba/classirlba_1_1MockMatrix.html#a8c39c466d1e59f539ec5f262c8692ac5',1,'irlba::MockMatrix::rows()'],['https://ltla.github.io/CppIrlba/classirlba_1_1ParallelSparseMatrix.html#aaf5317ab710d8135149c8c3e3d536a0d',1,'irlba::ParallelSparseMatrix::rows()']]]
 ];
